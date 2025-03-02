6c2dcf5d2c4055349534b07fb4669d2979ef7ff7
VikashKmr1106 <141212001+VikashKmr1106@users.noreply.github.com>
2025-03-02 16:45:20 +0530

    Update home.jsx

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,43 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import axios from 'axios';
-
+import React from 'react';
 import Sidebar from '../component/sidebar.jsx';
 import Feed from '../component/feed.jsx';
 import RightSidebar from '../component/rightSidebar.jsx';
 
-
-const home = () => {
-	const navigate = useNavigate();
-
-
-
-
+const Home = () => {
 	return (
-		<section className='w-auto min-h-screen bg-[#f5f5f5] flex items-center justify-center relative  p-4'>
-			
-				<div className='flex min-h-screen'>
-					{/* Sidebar */}
-					<div className='fixed'>
+		<section className='w-auto min-h-screen bg-[#f5f5f5] flex items-center justify-center relative p-4'>
+			<div className='flex min-h-screen'>
+				{/* Sidebar */}
+				<div className='fixed'>
 					<Sidebar />
-					</div>
-					
-
-					
-					<main className='flex-1 p-4 pt-0 max-w-3xl'>
-						<Feed />
-					</main>
-
-					{/* Right Sidebar */}
-					<div className="relative">
-						<RightSidebar/>
-					</div>
-					
 				</div>
-			) 
+
+				{/* Main Content */}
+				<main className='flex-1 p-4 pt-0 max-w-3xl ml-64'>
+					<Feed />
+				</main>
+
+				{/* Right Sidebar */}
+				<div className="relative">
+					<RightSidebar />
+				</div>
+			</div>
 		</section>
 	);
 };
 
-export default home;
+export default Home;
