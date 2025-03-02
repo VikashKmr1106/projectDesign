f09b70e90bcdc9cf4813983456cc68ce2f229915
VikashKmr1106 <141212001+VikashKmr1106@users.noreply.github.com>
2025-03-02 16:47:49 +0530

    Update home.jsx

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import axios from 'axios';
+
 import Sidebar from '../component/sidebar.jsx';
 import Feed from '../component/feed.jsx';
 import RightSidebar from '../component/rightSidebar.jsx';
 
-const Home = () => {
+const home = () => {
+	// const navigate = useNavigate();
+
 	return (
-		<section className='w-auto min-h-screen bg-[#f5f5f5] flex items-center justify-center relative p-4'>
+		<section className='w-auto min-h-screen bg-[#f5f5f5] flex items-center justify-center relative  p-4'>
 			<div className='flex min-h-screen'>
 				{/* Sidebar */}
 				<div className='fixed'>
 					<Sidebar />
 				</div>
 
-				{/* Main Content */}
-				<main className='flex-1 p-4 pt-0 max-w-3xl ml-64'>
+				<main className='flex-1 p-4 pt-0 max-w-3xl'>
 					<Feed />
 				</main>
 
 				{/* Right Sidebar */}
-				<div className="relative">
+				<div className='relative'>
 					<RightSidebar />
 				</div>
 			</div>
@@ -26,4 +31,4 @@ const Home = () => {
 	);
 };
 
-export default Home;
+export default home;
